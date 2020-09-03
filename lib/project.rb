758d174a418ab2dea87eec61f7bf615244a4bdfd
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 18:51:53 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+    
+  end
+  
+  def add_backer(backers)
+    @backers << backers
+    backer.backed_projects << self
+  end
+  
+end
